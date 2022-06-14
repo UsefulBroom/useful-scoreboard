135aae847c46a059e28e03daa465744180def4c6
SchmelzerDaniel-FS <dschmelzer8716@gmail.com>
2022-06-14 15:17:09 -0500

    Tailwind and React-Query
    Installed dependencies on project. Tailwind... is questionable

---
+++ useful-scoreboard/pages/index.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next'
+
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <h1 className='text-3xl font-bold underline'>
+        Hello
+      </h1>
+    </div>
+  )
+}
+
+export default Home
